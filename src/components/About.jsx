54240ce6bda60df4b2529127e31775cb3654b452
justinrasler <85715506+justinrasler@users.noreply.github.com>
2025-07-11 15:58:20 -0400

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,9 @@ const About = () => {
     <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
         <h1 className='text-2xl sm:text-4x1 font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 font-light'>Our Brand</span></h1>
         <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate about finding your dream home</p>
+        <div>
+            
+        </div>
     </div>
   )
 }

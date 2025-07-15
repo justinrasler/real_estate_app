63a4750f10369fba4032b236786fd1679cb2b83c
justinrasler <85715506+justinrasler@users.noreply.github.com>
2025-07-14 22:06:39 -0400

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden' id='Footer'>
+        <div className='container mx-auto flex flex-col md:flex-row justify-between items-start'>
+            <div className='w-full md:w-1/3 mb-8 md:mb-0'>
+                <img src={assets.logo_dark} alt="" />
+                <p className='text-gray-400 mt-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam accusamus blanditiis dolorum animi quo laudantium, in voluptatum est laboriosam amet laborum repudiandae ipsam quaerat tenetur. Porro accusantium voluptatum officia alias.</p>
+            </div>
+            <div className='w-full md:w-1/5 mb-8 md:mb-0'>
+                <h3 className='text-white text-lg font-bold mb-4'>Company</h3>
+                <ul className='flex flex-col gap-2 text-gray-400'>
+                    <a href="#Home" className='hover:text-white'>Home</a>
+                    <a href="#About Us" className='hover:text-white'>About Us</a>
+                    <a href="#Contact Us" className='hover:text-white'>Contact Us</a>
+                    <a href="#Privacy Policy" className='hover:text-white'>Privacy Policy</a>
+                </ul>
+            </div>
+            <div></div>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
